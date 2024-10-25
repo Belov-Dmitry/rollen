062f874c083c38c11142d5464558ec6f16d73f2e
Belov-Dmitry <91392446+Belov-Dmitry@users.noreply.github.com>
2024-10-26 00:09:57 +0300

    Initial comit
    Remove Storyboard

---
+++ Rollen/Rollen/App/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  Rollen
+//
+//  Created by Dmitry Belov on 25.10.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+          guard let windowScene = (scene as? UIWindowScene) else { return }
+          window = UIWindow(windowScene: windowScene)
+          let viewController = ViewController()
+          let navigationVC = UINavigationController(rootViewController: viewController)
+          window?.rootViewController = navigationVC
+          window?.makeKeyAndVisible()
+      }
+
+}
+
---
+++ Rollen/Rollen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rollen
+//
+//  Created by Dmitry Belov on 25.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
