64e3ac7e04eebde65dadc9c85160353cd6d04614
Belov-Dmitry <91392446+Belov-Dmitry@users.noreply.github.com>
2024-10-28 23:44:42 +0300

    Add some ViewControllers

---
+++ Rollen/Rollen/Modules/SideMenu/SideMenuViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SideMenuViewController.swift
+//  Rollen
+//
+//  Created by Dmitry Belov on 28.10.2024.
+//
+
+import UIKit
+
+class SideMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
