b2ce2034963dfe28432bcffb3ed61780afbf227f
Belov-Dmitry <91392446+Belov-Dmitry@users.noreply.github.com>
2024-11-01 23:22:56 +0300

    updated color scheme

---
+++ Rollen/Rollen/Coordinators/TabBarCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  TabBarCoordinator.swift
+//  Rollen
+//
+//  Created by Dmitry Belov on 31.10.2024.
+//
+
+import Foundation
--- Rollen/Rollen/Resources/AppColors.swift
+++ Rollen/Rollen/Resources/AppColors.swift
@@ -7,15 +7,15 @@
 
 import UIKit
 extension UIColor {
-    static var accentDarkBlue: UIColor { #colorLiteral(red: 0.002133060712, green: 0.19272843, blue: 0.2713456452, alpha: 1) }
-    static var accentBlue: UIColor { #colorLiteral(red: 0.00558962347, green: 0.2124502063, blue: 0.295145303, alpha: 1) }
+    static var accentDarkBlue: UIColor { #colorLiteral(red: 0.002133060712, green: 0.19272843, blue: 0.2713456452, alpha: 1) } //фон
+    static var accentBlue: UIColor { #colorLiteral(red: 0.00558962347, green: 0.2124502063, blue: 0.295145303, alpha: 1) } //таббар
     static var accentLightBlue: UIColor { #colorLiteral(red: 0.03202658892, green: 0.3842339516, blue: 0.4477190971, alpha: 1) }
 
-    static var accentDarkYellow: UIColor { #colorLiteral(red: 0.7158153653, green: 0.5656223893, blue: 0.1823150516, alpha: 1) }
-    static var accentYellow: UIColor { #colorLiteral(red: 0.9904655814, green: 1, blue: 0.2813923955, alpha: 1) }
+    static var accentDarkYellow: UIColor { #colorLiteral(red: 0.7158153653, green: 0.5656223893, blue: 0.1823150516, alpha: 1) } // не активная кнопка таббара
+    static var accentYellow: UIColor { #colorLiteral(red: 0.9904655814, green: 1, blue: 0.2813923955, alpha: 1) } //неактивная кнопка таббара
     static var accentLightYellow: UIColor { #colorLiteral(red: 0.7135325074, green: 0.6057977676, blue: 0.3245694935, alpha: 1) }
     
-    static var accentDarkRed: UIColor { #colorLiteral(red: 0.3345428109, green: 0.06248378009, blue: 0, alpha: 1) }
+    static var accentDarkRed: UIColor { #colorLiteral(red: 0.3345428109, green: 0.06248378009, blue: 0, alpha: 1) } //кнопка добавить
     static var accentRed: UIColor { #colorLiteral(red: 0.4299553037, green: 0.08417155594, blue: 0, alpha: 1) }
-    static var accentLightRed: UIColor { #colorLiteral(red: 0.7562223673, green: 0.3003486395, blue: 0.1867262721, alpha: 1) }
+    static var accentLightRed: UIColor { #colorLiteral(red: 0.7562223673, green: 0.3003486395, blue: 0.1867262721, alpha: 1) } //активная кнопка таббара
 }
